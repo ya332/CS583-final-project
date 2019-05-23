5506bc24acfaa25db6a08bb958fb3b52dc233cfc
Yigit Alparslan <yigit.alparslan@sap.com>
2019-05-22 21:20:15 -0400

    Complete baseline & GB image hashes

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
+import pickle
+
+DEFAULT_BASELINE_DCT = "baseline_dct.pkl"
+DEFAULT_BLURRED_DCT = "gb_dct.pkl"
+
 #Calculate DCT freqeuncy coefficients
 """
           N-1
@@ -61,26 +66,66 @@ def gaussianBlur(img,ksize=(5,5),sigma=10):
     cv2.GaussianBlur(src=img,dst=dst,ksize=ksize,sigmaX=0)
     return dst
 
-        
-
+def plotFace(original,blurred):
+    plt.subplot(121),plt.imshow(original,cmap=cm.Greys_r),plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(blurred,cmap=cm.Greys_r),plt.title('Gaussian Blurred')
+    plt.xticks([]), plt.yticks([])
+    return None
+    
+    
+"""
+# Load data (deserialize)
+with open('filename.pickle', 'rb') as handle:
+    unserialized_data = pickle.load(handle)
+"""
 if __name__=="__main__":
     images = readImages('img/')
     print('Found images:',len(images))
-    dct={}
+    baseline_dict,blurred_dict={},{}
+    
+    dirnames=['base_out','blurred_out']
+    for d in dirnames:
+        if d not in os.listdir(os.getcwd()):
+            os.mkdir(d) 
+            print('Creating output folder:',d)
+    
     for i in range(len(images)):
+        
         plt.figure(i)
-        dst = gaussianBlur(images[i])
-        plt.subplot(121),plt.imshow(images[i],cmap=cm.Greys_r),plt.title('Original')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(122),plt.imshow(dst,cmap=cm.Greys_r),plt.title('Gaussian Blurred')
-        plt.xticks([]), plt.yticks([])
-        imgResult = cv2.dct(images[i])
-        dct[i]=imgResult
-        img = np.uint8(imgResult*255.0)
-        print('Writing dct256_B'+str(i)+'.png...')
-        imageio.imwrite('dct256_B'+str(i)+'.png', img)
+        blurredImage = gaussianBlur(images[i])
+        plotFace(images[i],blurredImage)
+        baselineDCT, blurredDCT= cv2.dct(images[i]),cv2.dct(blurredImage)
+        baseline_dict[i]=baselineDCT
+        blurred_dict[i]=blurredDCT
+        
+        ####WRITE Baseline Images####
+        imgBase = np.uint8(baselineDCT*255.0)
+        print('Writing dct256_Base'+str(i)+'.png...')
+        dirname=dirnames[0]
+        imageio.imwrite(os.path.join(dirname,'dct256_Base'+str(i)+'.png'), imgBase)
+        
+        ###WRITE Baseline Coefficients###
+        # Store data (serialize)
+        with open(DEFAULT_BASELINE_DCT, 'wb') as fbase:
+            pickle.dump(baseline_dict, fbase, protocol=pickle.HIGHEST_PROTOCOL)
+
+        ####WRITE Blurred Images####
+        imgBlur = np.uint8(blurredDCT*255.0)
+        print('Writing dct256_Blurr'+str(i)+'.png...')
+        dirname=dirnames[1]
+        imageio.imwrite(os.path.join(dirname,'dct256_Blur'+str(i)+'.png'), imgBlur)
+        
+        ###WRITE Blurred Coefficients###
+        # Store data (serialize)
+        with open(DEFAULT_BLURRED_DCT, 'wb') as fblurred:
+            pickle.dump(blurred_dict, fblurred, protocol=pickle.HIGHEST_PROTOCOL)
+        
+            
+        
+        
     #WARNING:
-    plt.show() #--> Figures created through the pyplot interface will consume too much memory until explicitly closed.
+    #plt.show() #--> Figures created through the pyplot interface will consume too much memory until explicitly closed.
         
 
 
