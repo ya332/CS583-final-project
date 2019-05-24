049e710578c08a0d463f89e26c00e1160ca2521e
Yigit Alparslan <ya332@drexel.edu>
2019-05-24 16:39:52 -0400

    Annotate Images

--- img_transformation.py
+++ img_transformation.py
@@ -2,27 +2,46 @@ import os
 import imageio
 import numpy as np
 import cv2
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 import pickle
 from main import readImages
 dirname='annotated'
-images = readImages('img/')
-print(len(images))
 
 if dirname not in os.listdir(os.getcwd()):
     os.mkdir(dirname)
+#images = readImages('img/')
+#print(len(images))
+
+foldernames = os.listdir('img/')
+print(foldernames)
+#images = [imageio.imread('img/'+fn+'/image0.jpg')[::,::].astype(np.float32)/255. for fn in filenames]#glob.glob(imgFolder+'*.jpg')]
+###Image Annotation###
+i=0
+for f in foldernames:
     
+    img = Image.open('img/'+f+'/image0.jpg')
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(r'./sans-serif.ttf', 28)
+    draw.text((10,60),"Copyrighted",font=font)
+    draw.text((40,100),"For Test",font=font)
+    draw.text((20,140),"Purposes",font=font)
+    img.save(os.path.join(dirname,'image_annotated_'+str(i+1)+'.png'))
+    i+=1
+
+###Image Cropping###
+"""
 for i in range(len(images)):
     rows,cols = images[i].shape
-    overlay = np.copy(images[i])
-    output = np.copy(images[i])
-    alpha = 0.8
-    #textImg = np.zeros((rows,cols),np.uint8)
-    cv2.putText(overlay, "copyrighted", (0, cols//2), cv2.FONT_HERSHEY_SIMPLEX,0.85, (250,250,250),2,cv2.LINE_AA)
-    cv2.addWeighted(overlay,alpha,output,1-alpha,0,output)
+    
+    img = images[i]
+    draw = ImageDraw.Draw(img)
     #M = cv2.getRotationMatrix2D((cols/2,rows/2),45,1)
     #dst = cv2.warpAffine(textImg,M,(cols,rows))
     #images[i]+=textImg
-    imageio.imwrite(os.path.join(dirname,'image_annotated_'+str(i+1)+'.png'), output.astype(np.uint8))
-    
+    imageio.imwrite(os.path.join(dirname,'image_annotated_'+str(i+1)+'.png'), img)
+"""
