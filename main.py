456c41a5e6d00e2473895a43ded4a52956511646
Yigit Alparslan <ya332@drexel.edu>
2019-05-24 00:29:18 -0400

    Generate 180 rotated test images

--- main.py
+++ main.py
@@ -24,42 +24,6 @@ def readImages(imgFolder='img/'):
     images = [imageio.imread('img/'+fn+'/image0.jpg')[::,::].astype(np.float32)/255. for fn in filenames]#glob.glob(imgFolder+'*.jpg')]
     return images
 
-#Get DCT coefficients.
-def dct_1d(image, numberCoefficients=0):
-    
-    nc = numberCoefficients
-    n = len(image)
-    newImage= np.zeros_like(image).astype(float)
-
-  
-    for k in range(n):
-        sum = 0
-        for i in range(n):
-            sum += image[i] * cos(2 * pi * k / (2.0 * n) * i + (k * pi) / (2.0 * n))
-        ck = sqrt(0.5) if k == 0 else 1
-        newImage[k] = sqrt(2.0 / n) * ck * sum
-    
-    #Get top coefficients.
-    if nc > 0:
-        newImage.sort()
-        for i in range(nc, n):
-            newImage[i] = 0
-
-    return newImage
-
-def dct_2d(image, numberCoefficients=0):
-    
-    nc = numberCoefficients 
-    height,width = image.shape[0],image.shape[1]
-    imageRow = np.zeros_like(image).astype(float)
-    imageCol = np.zeros_like(image).astype(float)
-
-    for h in range(height):
-        imageRow[h, :] = dct_1d(image[h, :], nc) 
-    for w in range(width):
-        imageCol[:, w] = dct_1d(imageRow[:, w], nc) 
-
-    return imageCol
 def gaussianBlur(img,ksize=(5,5),sigma=10):
     #kernel = cv2.getGaussianKernel(ksize,sigma)
     dst = np.zeros_like(img)
